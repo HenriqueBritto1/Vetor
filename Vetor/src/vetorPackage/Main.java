64cba40902cfc885d3c36d7c664bb2902b7efb18
HenriqueBritto1 <91097589+HenriqueBritto1@users.noreply.github.com>
2022-09-21 17:47:12 -0400

    Add files via upload

---
+++ Vetor/src/vetorPackage/Main.java
@@ -0,0 +1,17 @@
+package vetorPackage;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int n = ler.nextInt();
+		
+		Vetor vet1 = new Vetor(n);
+        vet1.leVetor("V");
+        vet1.imprimeVetor("V");
+        vet1.somaElementos();
+        vet1.menorElemento();
+        
+	}
+
+}
---
+++ Vetor/src/vetorPackage/Vetor.java
@@ -0,0 +1,49 @@
+package vetorPackage;
+import java.util.Scanner;
+
+public class Vetor {
+	private int[] vet;
+    Scanner ler = new Scanner(System.in);
+    
+    public Vetor(int n){
+        vet = new int[n];
+    }
+    public int[] getVet() {
+		return vet;
+	}
+
+	public void setVet(int[] vet) {
+		this.vet = vet;
+	}
+	
+	public void leVetor(String V){
+	    for(int i = 0; i<vet.length;i++){
+	        vet[i] = ler.nextInt();
+	    }
+	}
+	
+	public void imprimeVetor(String V){
+	    for(int i = 0; i<vet.length;i++){
+	        System.out.print(vet[i] + " ");
+	    }
+	    System.out.printf("%n");
+	}
+	
+	public void somaElementos(){
+	    int soma = 0;
+	    for(int i =0;i<vet.length;i++){
+	        soma += vet[i];
+	    }
+	    System.out.println("soma = " + soma);
+	}
+	
+	public void menorElemento(){
+	    int menor = vet[0];
+	    for(int i =1;i<vet.length;i++){
+	        if(menor>vet[i]){
+	            menor = vet[i];
+	        }
+	    }
+	    System.out.println("menor = "+ menor);
+	}
+}
